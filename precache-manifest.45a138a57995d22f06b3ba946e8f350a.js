cf62485a2ad918a17c26de8c1a71db19ddcb1a2a
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-09-16 18:46:37 +0300

    Updates

---
+++ precache-manifest.45a138a57995d22f06b3ba946e8f350a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cb0e8239b99b8658e61522f94d8fc4",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "b0389fa86e9bf00239bb",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.b18376dc.chunk.css"
+  },
+  {
+    "revision": "a6bb1633070411ba45a2",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.a1ac5799.chunk.js"
+  },
+  {
+    "revision": "b0389fa86e9bf00239bb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.865f6269.chunk.js"
+  },
+  {
+    "revision": "db10479c1573f6125dd1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0eafb310.js"
+  }
+]);
